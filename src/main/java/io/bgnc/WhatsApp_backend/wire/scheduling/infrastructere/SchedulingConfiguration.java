cfdc9202c0ae27eaefaca87deb7d6b3b9dbbdd9b
BGNC <bugra34055@hotmail.com>
2024-07-03 20:02:15 +0300

    database config and scheduling and corsfilter configuration is done.

---
+++ src/main/java/io/bgnc/WhatsApp_backend/wire/scheduling/infrastructere/SchedulingConfiguration.java
@@ -0,0 +1,7 @@
+package io.bgnc.WhatsApp_backend.wire.scheduling.infrastructere;
+
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+@EnableScheduling
+public class SchedulingConfiguration {
+}
