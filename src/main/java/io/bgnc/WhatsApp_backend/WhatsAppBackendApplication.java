1ad53693ec8c3b324ff5cb2978143926085975b4
BGNC <bugra34055@hotmail.com>
2024-07-03 17:50:38 +0300

    whatsapp backend project is created

---
+++ src/main/java/io/bgnc/WhatsApp_backend/WhatsAppBackendApplication.java
@@ -0,0 +1,13 @@
+package io.bgnc.WhatsApp_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WhatsAppBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WhatsAppBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/bgnc/WhatsApp_backend/WhatsAppBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package io.bgnc.WhatsApp_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WhatsAppBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
